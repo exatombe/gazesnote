5e77936710d994fcbd27a16a7e8710277cdfd64f
garder500 <jeremy27.clara22@gmail.com>
2024-03-05 00:00:05 +0100

    Adding automatic Theme Switch (Light or dark based on Device)

--- App.tsx
+++ App.tsx
@@ -1,11 +1,8 @@
 import * as React from 'react';
-import { PaperProvider } from 'react-native-paper';
 import Router from './src/Router';
 
 export default function App() {
   return (
-    <PaperProvider>
       <Router />
-    </PaperProvider>
   );
 }
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,17 +1,24 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { DarkTheme, NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './Home';
+
+import {useColorScheme} from 'react-native';
+import { MD3DarkTheme, MD3LightTheme, PaperProvider } from 'react-native-paper';
 
 const Stack = createNativeStackNavigator();
 
 export default function Router() {
-    return (
-        <NavigationContainer>
+    const colorsScheme = useColorScheme();
+    return (                    
+    <PaperProvider theme={colorsScheme === 'dark' ? MD3DarkTheme : MD3LightTheme}>
+        <NavigationContainer theme={colorsScheme === 'dark' ? DarkTheme : DefaultTheme}>
             <Stack.Navigator screenOptions={{
                 headerShown: false
             }}>
                 <Stack.Screen name="Home" component={Home} />
             </Stack.Navigator>
-        </NavigationContainer>
+        </NavigationContainer>                
+    </PaperProvider>
+
     );
 }
